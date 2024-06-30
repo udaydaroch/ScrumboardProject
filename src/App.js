c96827ca06495c195b0a146adac7d666c3845363
Uday <Uda12@uclive.ac.nz>
2024-06-30 13:17:37 +1200

    basic changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 // App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Navbar from './Navbar';
-import LoginPage from './LoginPage';
-import Scrumboard from './Scrumboard';
-import StaffList from './StaffList';
+import Navbar from './Pages/Navbar';
+import LoginPage from './Pages/LoginPage';
+import Scrumboard from './Pages/Scrumboard';
+import StaffList from './Pages/StaffList';
+import SetupBoard from './Pages/SetupBoard';
 
 const App = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false); // New state to check if the user is admin
 
-    const handleLogin = () => {
+    const handleLogin = (username) => {
         setIsLoggedIn(true);
+        console.log(username);
+        setIsAdmin(username === 'admin'); // Set admin status based on the username
     };
 
     const handleLogout = () => {
         setIsLoggedIn(false);
+        setIsAdmin(false);
     };
 
     return (
         <Router>
             {isLoggedIn ? (
                 <>
-                    <Navbar onLogout={handleLogout} />
+                    <Navbar onLogout={handleLogout} isAdmin={isAdmin} />
                     <Routes>
                         <Route path="/scrumboard" element={<Scrumboard />} />
                         <Route path="/stafflist" element={<StaffList />} />
+                        {isAdmin && <Route path="/setupboard" element={<SetupBoard />} />} {/* Admin route */}
                         <Route path="*" element={<Navigate to="/scrumboard" />} />
                     </Routes>
                 </>
