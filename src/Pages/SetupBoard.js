0e7a9f66fd1d80607f46fc6ae3675964de90bedc
Uday <Uda12@uclive.ac.nz>
2024-07-09 12:33:35 +1200

    fixed assigning bug

--- src/Pages/SetupBoard.js
+++ src/Pages/SetupBoard.js
@@ -127,7 +127,7 @@ const SetupBoard = () => {
             }
         };
 
-        axios.post(`https://scrumboard-project-back-end.vercel.app/setUpBoard`, scrumboardData, {
+        axios.post(`https://localhost:5432/setUpBoard`, scrumboardData, {
             headers: {
                 'Content-Type': 'application/json',
                 'X-Authorization': token,
